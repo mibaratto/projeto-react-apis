7ad3a72fbf03f1e2e6e73df1f5372121f1620c7e
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-03-17 00:07:49 +0100

    creating card whith chackra

--- pokedex-app/src/Components/PokemonCard/PokemonCard.js
+++ pokedex-app/src/Components/PokemonCard/PokemonCard.js
@@ -1,8 +1,11 @@
-import { Button } from "@chakra-ui/react";
+import { Button, HStack, Image, Stack, Text } from "@chakra-ui/react";
 import { PokemonCardContainer } from "./PokemonCardStyled";
 import { useLocation, useNavigate } from "react-router-dom";
 import { goToPokemonDetailPage } from "../../Routes/coordinator";
 import { Box } from "@chakra-ui/react";
+
+import { Flex, Spacer } from '@chakra-ui/react'
+
 
 export function PokemonCard({pokemon, addToPokedex, removeFromPokedex}) {
     const navigate = useNavigate(pokemon.id)
@@ -20,23 +23,36 @@ export function PokemonCard({pokemon, addToPokedex, removeFromPokedex}) {
     }
 
     return (
-            <PokemonCardContainer>
-                <Box bg={backgroundColor}>
-                <img src={pokemon.image} alt=""></img>
-                <h1>{pokemon.name}</h1>
-                <h1>{pokemon.id}</h1>
-                <h1>{pokemon.types}</h1>
-                <div>
+        <Flex  m="10px" direction="column" pos="relative" border="1px">
+            <Box  bg="red" opacity="0.0">1</Box>
+            <Box flexGrow={1}>
+            <HStack bg={backgroundColor} alignItems="baseline">
+                <Stack border="1px" ml="8px" >
+                    <Text as="p">{pokemon.id}</Text>
+                    <Text>{pokemon.name}</Text>
+                    <Text>{pokemon.types}</Text>
+                </Stack>
+                <Spacer/>
+                <Stack border="1px" position="absolute" top="0" right="0" >
+                    <Image src={pokemon.image} alt=""  boxSize='100px' mr="12px"></Image>
+                </Stack>
+            </HStack>
+            </Box>
+            <HStack  bg={backgroundColor}>
+                <Stack>
                     {location.pathname === "/" ? (
-                        <Button onClick={() => addToPokedex(pokemon)} >Adiciona Pokedex</Button>
-                    ) : (
-                        <Button onClick={() => removeFromPokedex(pokemon)} >Remove Pokedex</Button>
-                    )}
-                </div>   
-                <Button onClick={() => goToPokemonDetailPage(navigate, pokemon.id)} >Detalhe</Button>
-                </Box>
-                
-            </PokemonCardContainer>
-            
-    )
+                    <Button onClick={() => addToPokedex(pokemon)} >Adiciona Pokedex</Button>
+                        ) : (
+                    <Button onClick={() => removeFromPokedex(pokemon)} >Remove Pokedex</Button>
+                        )}
+                </Stack>
+                    <Spacer/>
+                <Stack>
+                    <Button onClick={() => goToPokemonDetailPage(navigate, pokemon.id)} >Detalhe</Button>  
+                </Stack>        
+            </HStack>
+        </Flex>
+
+
+                )
 }
