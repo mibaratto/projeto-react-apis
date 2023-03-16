468196733d20af09a5d15ee6dfd6a6f06e06b886
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-03-16 15:14:27 +0100

    rendering baseStats at detail page

--- pokedex-app/src/Pages/PokemonDetailPage/PokemonDetailPage.js
+++ pokedex-app/src/Pages/PokemonDetailPage/PokemonDetailPage.js
@@ -7,14 +7,34 @@ export function PokemonDetailPage({pokemonList}) {
     const { id } = useParams()
 
     const pokemonDetalhe = pokemonList.find(pokemon => pokemon.id === Number(id))
+    const movesMaped = pokemonDetalhe.moves
+        .slice(0, 7)
+        .map((move) => (<p>{move}</p>))
+
+    const baseStasNameMaped = pokemonDetalhe.baseStats.map((baseStat) => (<p>{baseStat.name}</p>))
+    const baseStasBaseStasMaped = pokemonDetalhe.baseStats.map((baseStat) => (<p>{baseStat.baseStat}</p>))
+
+
+
+    // console.log("movesmovesMaped", movesMaped)
+    // console.log("moves", pokemonDetalhe.moves )
 
     return (
             <PokemonDetailContainer>
+                <img src={pokemonDetalhe.image} alt="pokemon"></img>
+                <img src={pokemonDetalhe.spritesFront} alt="pokemon"></img>
+                <img src={pokemonDetalhe.spritesBack} alt="pokemon"></img>
+                <>{pokemonDetalhe.name}</>
+                <>{pokemonDetalhe.id}</>
+                <>{pokemonDetalhe.types}</>
+                <>{movesMaped}</>
+                <div>
+                    {baseStasNameMaped}
+                </div>
+                <div>
+                    {baseStasBaseStasMaped}
+                </div>
 
-                <img src={pokemonDetalhe.image} alt=""></img>
-                <h1>{pokemonDetalhe.name}</h1>
-                <h1>{pokemonDetalhe.id}</h1>
-                <h1>{pokemonDetalhe.types}</h1>
             </PokemonDetailContainer>
             
     )
