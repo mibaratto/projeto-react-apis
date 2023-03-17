953139dee50e93e46e2c27c2ce84c7bcff982d95
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-03-17 10:43:41 +0100

    adding type flags

--- pokedex-app/src/Components/PokemonCard/PokemonCard.js
+++ pokedex-app/src/Components/PokemonCard/PokemonCard.js
@@ -3,6 +3,12 @@ import { PokemonCardContainer } from "./PokemonCardStyled";
 import { useLocation, useNavigate } from "react-router-dom";
 import { goToPokemonDetailPage } from "../../Routes/coordinator";
 import { Box } from "@chakra-ui/react";
+import bug from "../../assets/bug.png"
+import grass from "../../assets/grass.png";
+import fire from "../../assets/fire.png";
+import water from "../../assets/water.png";
+import normal from "../../assets/normal.png";
+import logo from "../../assets/logo.png";
 
 import { Flex, Spacer } from '@chakra-ui/react'
 
@@ -22,6 +28,19 @@ export function PokemonCard({pokemon, addToPokedex, removeFromPokedex}) {
         }
     }
 
+    // const tagTipe = () => {
+    //     switch (pokemon.types[0]) {
+    //         case "grass": return grass;
+    //         case "fire": return fire;
+    //         case "water": return water;
+    //         case "bug": return bug;
+    //         case "normal": return normal;
+    //         default: return "not found"
+    //     }
+    // }
+
+    console.log("tag" , grass, "bug", bug)
+
     return (
         <Flex  m="10px" direction="column" pos="relative" border="1px">
             <Box  bg="red" opacity="0.0">1</Box>
@@ -30,7 +49,8 @@ export function PokemonCard({pokemon, addToPokedex, removeFromPokedex}) {
                 <Stack border="1px" ml="8px" >
                     <Text as="p">{pokemon.id}</Text>
                     <Text>{pokemon.name}</Text>
-                    <Text>{pokemon.types}</Text>
+                    <img src="" alt="tipo pokemon"/>
+                    
                 </Stack>
                 <Spacer/>
                 <Stack border="1px" position="absolute" top="0" right="0" >
